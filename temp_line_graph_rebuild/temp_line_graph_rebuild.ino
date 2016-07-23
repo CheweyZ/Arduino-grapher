aa917a460a5c88a58c2cecfcd9b7441ad46693d9
CheweyZ <cheweyzak002@gmail.com>
2016-07-23 16:10:19 +1200

    patch
    all all seperate between hour and week now

--- temp_line_graph_rebuild/temp_line_graph_rebuild.ino
+++ temp_line_graph_rebuild/temp_line_graph_rebuild.ino
@@ -19,7 +19,7 @@ MCUFRIEND_kbv tft;
 
 //#include <stdint.h>
 //___________________________________
-//#include "TouchScreen.h"
+#include "TouchScreen.h"
 //____________________________________
 /*#if defined(__SAM3X8E__)
   #undef __FlashStringHelper::F(string_literal)
@@ -37,28 +37,28 @@ MCUFRIEND_kbv tft;
   if ('0' <= *p && *p <= '9') v = *p - '0';
   return 10 * v + *++p - '0';
   }*/
-/*
+
 // These are the pins for the shield!
 #define YP A1  // must be an analog pin, use "An" notation!
 #define XM A2  // must be an analog pin, use "An" notation!
 #define YM 7   // can be a digital pin
 #define XP 6   // can be a digital pin
 //uint8_t SwapXY = 0;
-
-uint16_t TS_LEFT = 920;
-uint16_t TS_RT  = 150;
-uint16_t TS_TOP = 940;
-uint16_t TS_BOT = 120;
 /*
-#define MINPRESSURE 10
-#define MAXPRESSURE 1000*/
+  uint16_t TS_LEFT = 920;
+  uint16_t TS_RT  = 150;
+  uint16_t TS_TOP = 940;
+  uint16_t TS_BOT = 120;
+
+  #define MINPRESSURE 10
+  #define MAXPRESSURE 1000*/
 
 
 
 // For better pressure precision, we need to know the resistance
 // between X+ and X- Use any multimeter to read it
 // For the one we're using, its 300 ohms across the X plate
-//TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 #define BLACK   0x0000
 #define BLUE    0x001F
@@ -138,13 +138,13 @@ void loop() {
     int myHeight = tft.height();
     //tft.fillRect(0, 0, myWidth, myHeight/4, BLACK);
     tft.setTextColor(WHITE, BLACK);
-   /* tft.setCursor((myWidth * 6 / 8) / 3, myHeight / 8);
-    if (second() == 0) {
-      tft.fillRect(0, 0, myWidth, myHeight / 4, BLACK); //as wierd stuff happens if this is not here the placement of the second jumps one and leaves the 9 from 59
-      // above the line fills to make the refresh as seamless as possible
+    /* tft.setCursor((myWidth * 6 / 8) / 3, myHeight / 8);
+      if (second() == 0) {
+       tft.fillRect(0, 0, myWidth, myHeight / 4, BLACK); //as wierd stuff happens if this is not here the placement of the second jumps one and leaves the 9 from 59
+       // above the line fills to make the refresh as seamless as possible
 
-    }
-    tft.print(second());*/
+      }
+      tft.print(second());*/
 
     tft.drawFastHLine(0, myHeight / 4, myWidth, linecol);
     tft.drawFastVLine(myWidth / 3, 0, myHeight / 4, linecol);
@@ -152,12 +152,12 @@ void loop() {
     //tft.drawFastHLine(0, (myHeight*7)/8, myWidth, linecol);  works use for weeks
 
     if (currentview == 1) {
-     // tft.drawFastHLine(0, (myHeight - 12), myWidth, linecol);
+      // tft.drawFastHLine(0, (myHeight - 12), myWidth, linecol);
       howmanyaccross = 15;
       multiplier = 15;
     } else if (currentview == 2) {
-     // tft.setCursor(0, (myHeight - 15));
-     // tft.drawFastHLine(0, (myHeight * 7) / 8, myWidth, linecol);
+      // tft.setCursor(0, (myHeight - 15));
+      // tft.drawFastHLine(0, (myHeight * 7) / 8, myWidth, linecol);
       howmanyaccross = 7;
       multiplier = 1;
     }
@@ -183,199 +183,199 @@ void loop() {
       //masterfile = SD.open("testh.txt", FILE_WRITE);
       //masterfile.seek(0);
 
-      writer("hh",dht.humidity);
-      writer("ht",dht.temperature_C);
+      writer("hh", dht.humidity);
+      writer("ht", dht.temperature_C);
 
       grapher();
-    } else if ((minute() == 00) && (second() == 0)) {
+    }
+    if ((minute() == 00) && (second() == 10)) {
       weekavg("h");
       weekavg("t");
       grapher();
-      }
+    }
 
 
 
 
-      tft.setCursor((myWidth / 8) / 3, myHeight / 8);
-      tft.print(hour());
-      tft.setCursor((myWidth * 3.5 / 8) / 3, myHeight / 8);
-      tft.print(minute());
+    tft.setCursor((myWidth / 8) / 3, myHeight / 8);
+    tft.print(hour());
+    tft.setCursor((myWidth * 3.5 / 8) / 3, myHeight / 8);
+    tft.print(minute());
 
-     /* if (dht.humidity > highhumid) {
-        tft.setTextColor(humidhigh, BLACK);
+    /* if (dht.humidity > highhumid) {
+       tft.setTextColor(humidhigh, BLACK);
       } else if (dht.humidity > warnhumid) {
-        tft.setTextColor(humidwarn, BLACK);
+       tft.setTextColor(humidwarn, BLACK);
       } else {*/
-        tft.setTextColor(humidcol, BLACK);
-      //}
-      //tft.setTextColor(humidcol,BLACK);
-      tft.setCursor((myWidth / 3) + 20, myHeight / 8);
-      tft.print(dht.humidity);
-      /*if (dht.temperature_C > hightemp) {
-        tft.setTextColor(temphigh, BLACK);
+    tft.setTextColor(humidcol, BLACK);
+    //}
+    //tft.setTextColor(humidcol,BLACK);
+    tft.setCursor((myWidth / 3) + 20, myHeight / 8);
+    tft.print(dht.humidity);
+    /*if (dht.temperature_C > hightemp) {
+      tft.setTextColor(temphigh, BLACK);
       } else if (dht.temperature_C > warntemp) {
-        tft.setTextColor(tempwarn, BLACK);
+      tft.setTextColor(tempwarn, BLACK);
       } else {*/
-        tft.setTextColor(tempcol, BLACK);
-     // }
-      tft.setCursor(((2 * myWidth) / 3) + 20, myHeight / 8);
-      tft.print(dht.temperature_C);
+    tft.setTextColor(tempcol, BLACK);
+    // }
+    tft.setCursor(((2 * myWidth) / 3) + 20, myHeight / 8);
+    tft.print(dht.temperature_C);
 
-      if (second() == 15) {
-        currentview += 1;
-        if (currentview == 3) {
-          currentview -= 2;
-        }
-        grapher();
+    if (second() == 15) {
+      currentview += 1;
+      if (currentview == 3) {
+        currentview -= 2;
       }
-
-
-
-      //Serial.println(openspace);
-      //Serial.println(myHeight); //240
-      //Serial.println(myWidth); //320
-      oldsecond = second();
+      grapher();
     }
-    //delay(3000);
 
+
+
+    //Serial.println(openspace);
+    //Serial.println(myHeight); //240
+    //Serial.println(myWidth); //320
+    oldsecond = second();
   }
+  //delay(3000);
 
-  void grapher() {
-    tft.fillScreen(BLACK);
-    if (currentview == 1) {
-      shifter("hh", "h");
-      shifter("ht", "t");
-      outgraph("h", 15, BLUE);
-      outgraph("t", 15, GREEN);
-    } else {
-      shifter("wh", "wlh");
-      shifter("wt", "wlt");
-      outgraph("wlh", 7, BLUE);
-      //outgraph("wlt", 7, GREEN);
+}
+
+void grapher() {
+  tft.fillScreen(BLACK);
+  if (currentview == 1) {
+    shifter("hh", "h");
+    shifter("ht", "t");
+    outgraph("h", 15, BLUE);
+    outgraph("t", 15, GREEN);
+  } else {
+    shifter("wh", "wlh");
+    shifter("wt", "wlt");
+    outgraph("wlh", 7, BLUE);
+    outgraph("wlt", 7, GREEN);
+  }
+}
+
+
+void writer(String fileto, float data) {
+  masterfile = SD.open(String(fileto + ".csv"), FILE_WRITE);
+  masterfile.println(data);
+  masterfile.close();
+}
+
+
+void weekavg(String fileto) {
+  holder3 = 0;
+  holder = 0;
+  masterfile = SD.open(String(fileto + ".csv"));
+  while (masterfile.available()) {
+    holder2 = masterfile.readStringUntil('\n');
+    holder += holder2.toInt();
+    holder3++;
+  }
+  masterfile.close();
+  writer((String("w" + fileto)), (holder / 15));
+}
+
+
+
+void outgraph(String fileto, int howmany, int col) {
+  int myWidth = tft.width();
+  int myHeight = tft.height();
+  int drawpointx = 0;
+  int drawpointy = 0;
+  masterfile = SD.open(String(fileto + ".csv"));
+  for (int upto = 0; upto <= howmany; upto++) {
+    //Serial.println(masterfile.read());
+    //masterfile.seek(upto);
+    //holder=masterfile.read(buffer,sizeof(buffer));
+    holder2 = masterfile.readStringUntil('\n');
+    holder = holder2.toInt();
+    // masterfile.read();
+    //holder=20;
+    // Serial.println(holder);
+    // Serial.write(masterfile.read());
+    // if (currentview == 1) {
+    //tft.drawLine(x1, y1, x2, y2, color);
+    spacetilltop = map(holder, 0, 100, myHeight - 15, myHeight / 4);
+    //tft.drawLine((myWidth * (upto/2) / howmanyaccross),(((myWidth * (upto/2)) / howmanyaccross)*2),spacetilltop,spacetilltop,BLUE);
+    // tft.drawLine(drawpointx, drawpointy, (myWidth * (upto / 2) / howmany), spacetilltop, col);
+    tft.drawLine(drawpointx, drawpointy, ((myWidth * upto) / howmany), spacetilltop, col);
+    // drawpointx = (myWidth * (upto / 2) / howmany);
+    drawpointx = (myWidth * upto) / howmany;
+    drawpointy = spacetilltop;
+    //}
+    if (!masterfile.available()) {
+      break;
     }
   }
+  masterfile.close();
+}
 
 
-  void writer(String fileto,float data){
-    masterfile = SD.open(String(fileto + ".csv"), FILE_WRITE);
-     masterfile.println(data);
-     masterfile.close();    
+
+
+void shifter(String fileto, String newfile) {
+  SD.remove(String(newfile + ".csv"));
+  int temp = 0;
+  int temp2 = 0;
+  //Serial.println("it");
+  masterfile = SD.open(String(fileto + ".csv"));
+  //masterfile.seek(20);
+  while (masterfile.available()) {
+    masterfile.readStringUntil('\n');
+    temp++;
+    //Serial.println("works");
   }
+  masterfile.close();
 
 
-  void weekavg(String fileto){
-      holder3=0;
-      masterfile = SD.open(String(fileto+".csv"));
-      while (masterfile.available()) {
-        holder2=masterfile.readStringUntil('\n');
-        holder+=holder2.toInt();
-        holder3++;
-      }
-      masterfile.close();
-      writer(String("w"+fileto),(holder/holder3));
-  }
-
-
-
-  void outgraph(String fileto, int howmany, int col) {
-    int myWidth = tft.width();
-    int myHeight = tft.height();
-    int drawpointx = 0;
-    int drawpointy = 0;
+  // masterfile = SD.open(String(fileto + ".txt"));
+  for (int count = 0; count < 16; count++) {
     masterfile = SD.open(String(fileto + ".csv"));
-    for (int upto = 0; upto <= howmany; upto++) {
-      //Serial.println(masterfile.read());
-      //masterfile.seek(upto);
-      //holder=masterfile.read(buffer,sizeof(buffer));
-      holder2 = masterfile.readStringUntil('\n');
-      holder = holder2.toInt();
-      // masterfile.read();
-      //holder=20;
-      // Serial.println(holder);
-      // Serial.write(masterfile.read());
-      // if (currentview == 1) {
-      //tft.drawLine(x1, y1, x2, y2, color);
-      spacetilltop = map(holder, 0, 100, myHeight - 15, myHeight / 4);
-      //tft.drawLine((myWidth * (upto/2) / howmanyaccross),(((myWidth * (upto/2)) / howmanyaccross)*2),spacetilltop,spacetilltop,BLUE);
-      // tft.drawLine(drawpointx, drawpointy, (myWidth * (upto / 2) / howmany), spacetilltop, col);
-      tft.drawLine(drawpointx, drawpointy, ((myWidth * upto) / howmany), spacetilltop, col);
-      // drawpointx = (myWidth * (upto / 2) / howmany);
-      drawpointx = (myWidth * upto) / howmany;
-      drawpointy = spacetilltop;
-      //}
-      if (!masterfile.available()) {
-        break;
-      }
-    }
-    masterfile.close();
-  }
-
-
-
-
-  void shifter(String fileto, String newfile) {
-    SD.remove(String(newfile + ".csv"));
-    int temp = 0;
-    int temp2 = 0;
-    //Serial.println("it");
-    masterfile = SD.open(String(fileto + ".csv"));
-    //masterfile.seek(20);
+    temp2 = 0;
     while (masterfile.available()) {
-      masterfile.readStringUntil('\n');
-      temp++;
-      //Serial.println("works");
-    }
-    masterfile.close();
-
-
-    // masterfile = SD.open(String(fileto + ".txt"));
-    for (int count = 0; count < 16; count++) {
-      masterfile = SD.open(String(fileto + ".csv"));
-      temp2 = 0;
-      while (masterfile.available()) {
-        if (temp2 == (temp - count)) {
-          holder2 = masterfile.readStringUntil('\n');
-          holder = holder2.toInt();
-          //Serial.println("it");
-          history[count] = holder;
-          //Serial.println(history[count]);
-          //Serial.println("works");
-        }
-        masterfile.readStringUntil('\n');
-        temp2++;
+      if (temp2 == (temp - count)) {
+        holder2 = masterfile.readStringUntil('\n');
+        holder = holder2.toInt();
+        //Serial.println("it");
+        history[count] = holder;
+        //Serial.println(history[count]);
+        //Serial.println("works");
       }
-      masterfile.close();
-      //history[temp]=holder;
-      //Serial.println(history[temp]);
-      //temp++;
-
+      masterfile.readStringUntil('\n');
+      temp2++;
     }
     masterfile.close();
-    /*
-      Serial.println("_");
-      Serial.println(history[0]);
-      Serial.println(history[15]);
-      Serial.println(history[18]);
-    */
-    //Serial.println(sizeof(history));
-    //Serial.println(history[50]);
-
-    masterfile = SD.open(String(newfile + ".csv"), FILE_WRITE);
-    for (int c = 0; c < 15; c++) {
-      masterfile.println(history[c]);
-      //Serial.println(history[c]);
-      //masterfile.flush();
-
-    }
-    masterfile.close();
-
+    //history[temp]=holder;
+    //Serial.println(history[temp]);
+    //temp++;
 
   }
-
-
+  masterfile.close();
   /*
-    masterfile = SD.open("testh.txt", FILE_WRITE);
-    //masterfile.seek(0);
-    masterfile.println(int(dht.humidity));
+    Serial.println("_");
+    Serial.println(history[0]);
+    Serial.println(history[15]);
+    Serial.println(history[18]);
   */
+  //Serial.println(sizeof(history));
+  //Serial.println(history[50]);
+
+  masterfile = SD.open(String(newfile + ".csv"), FILE_WRITE);
+  for (int c = 0; c < 15; c++) {
+    masterfile.println(history[c]);
+    //Serial.println(history[c]);
+    //masterfile.flush();
+
+  }
+  masterfile.close();
+}
+
+
+/*
+  masterfile = SD.open("testh.txt", FILE_WRITE);
+  //masterfile.seek(0);
+  masterfile.println(int(dht.humidity));
+*/
 
